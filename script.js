805670eba9b3c0af1ff65f287d8d7b2327d8b020
RyanGlem <ryanglem@hotmail.com>
2021-01-07 14:20:52 -0500

    validate

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ console.log(part2)
 part2.addEventListener("mouseover", () => {
     alert("Hey, I told you not to hover over me!")
 })
+
+function validate () {
+
+    var pass = document.getElementById ("pw")
+
+    if (pass.value != 12345678) {
+
+        var h1 = document.getElementsByTagName ("h1")
+        h1[0].innerHTML = "NonBook Login"
+
+    } else {
+
+        alert("Password is incorrect")
+
+    }
+
+}
